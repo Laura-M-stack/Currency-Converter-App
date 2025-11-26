ebfba5f91aa15930d1628cedc1af591e2d75ffc3
Laura Moyano <lauraymh000@hotmail.com>
2025-11-26 16:13:15 -0300

    Configure currency converter for GitHub Pages

--- currency-converter/vite.config.ts
+++ currency-converter/vite.config.ts
@@ -1,17 +1,14 @@
-
 import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import sveltePreprocess from 'svelte-preprocess';
+import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [
     svelte({
-      preprocess: sveltePreprocess({
-        scss: {
-          includePaths: ['src'],
-        },
-      }),
+      preprocess: vitePreprocess(),
     }),
   ],
+  base: '/Currency-Converter-App/',
+  build: {
+    outDir: '../docs',
+  },
 });
-
